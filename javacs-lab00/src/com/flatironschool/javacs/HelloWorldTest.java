9e87f39cc9ba23f91de46aad435e9928e4c7ea34
EricaTuLuan <tluan@brynmawr.edu>
2016-04-16 23:15:57 -0400

    1
    1

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorldTest.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorldTest.java
@@ -6,10 +6,10 @@ import static org.junit.Assert.assertTrue;
  * Created by pletcher on 3/10/16.
  */
 public class HelloWorldTest {
-
+   
     @org.junit.Test
     public void testGetVersion() throws Exception {
-        HelloWorld main = new HelloWorld();
+         HelloWorld main = new HelloWorld();
         assertTrue(main.getVersion() >= 1.7);
     }
 }
